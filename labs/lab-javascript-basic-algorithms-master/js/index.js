a2e30a0c3f0665f56ae7b438694c2c8a377e728d
krystel <krystel.dev@gmail.com>
2022-11-14 20:56:31 +0100

    lab 3 js

--- labs/lab-javascript-basic-algorithms-master/js/index.js
+++ labs/lab-javascript-basic-algorithms-master/js/index.js
@@ -1,5 +1,60 @@
-// Iteration 1: Names and Input
 console.log("I'm ready!");
+
+// Iteration 1: Names and Input
+// 1.1 Create a variable hacker1 with the driver's name.
+let hacker1 = "Damian";
+// 1.2 Print "The driver's name is XXXX".
+console.log(`The driver's name is ${hacker1}`);
+// 1.3 Create a variable hacker2 with the navigator's name.
+let hacker2 = "Chrome";
+// 1.4 Print "The navigator's name is YYYY".
+console.log(`The navigator's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
+// 2.1. Depending on which name is longer, print:
+// - The driver has the longest name, it has XX characters. or
+// - It seems that the navigator has the longest name, it has XX characters. or
+// - Wow, you both have equally long names, XX characters!.
+
+if (hacker1.length === hacker2.length) {
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters!.`
+  );
+} else if (hacker1.length < hacker2.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else {
+  console.log(
+    `The driver has the longest name, it has ${hacker2.length} characters.`
+  );
+}
 
 // Iteration 3: Loops
+// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "J O H N"
+let hacker1UpperCase = hacker1.toUpperCase();
+let hacker1UpperCaseSpace = [];
+let reckah2 = [];
+
+for (let i = 0; i < hacker1.length; i++) {
+  hacker1UpperCaseSpace.push(hacker1[i].toUpperCase());
+}
+console.log(hacker1UpperCaseSpace.join(" "));
+// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  reckah2.push(hacker2[i]);
+}
+console.log(reckah2.join(""));
+
+// 3.3 Depending on the lexicographic order of the strings, print:
+// - The driver's name goes first.
+// - Yo, the navigator goes first definitely.
+// - What?! You both have the same name?
+if (hacker1 > hacker2) {
+  console.log("Yo, the navigator goes first definitely.");
+} else if (hacker1 < hacker2) {
+  console.log("The driver's name goes first.");
+} else {
+  console.log("What?! You both have the same name?");
+}
