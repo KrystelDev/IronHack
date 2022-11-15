74c7fbb27bae3f2b6bab13094a6e91214266ba2a
krystel <krystel.dev@gmail.com>
2022-11-15 12:01:07 +0100

    lab js NOT FINISH bonus 3/3

--- labs/lab-javascript-basic-algorithms-master/js/indexBonus.js
+++ labs/lab-javascript-basic-algorithms-master/js/indexBonus.js
@@ -27,16 +27,6 @@ document.body.childNodes[15].innerHTML = `La palabra "et", del latín, aparece $
 
 // ¿Son cap i cua?
 let phraseToCheck = [];
-let sdrow = [];
-let drow1 = [];
-let drow2 = [];
-let drow3 = [];
-let drow4 = [];
-let drow5 = [];
-let drow6 = [];
-let drow7 = [];
-let drow8 = [];
-let drow9 = [];
 
 let stringComprobar = [
   "A man, a plan, a canal, Panama!",
@@ -49,69 +39,30 @@ let stringComprobar = [
   "Was it a car or a cat I saw?",
   "No 'x' in Nixon",
 ];
+let stringComprobarClear = [];
+let stringComprobarClearInvertido = [];
 
+// Limpiaremos los strings para poder trabajar con ellos y tendremos uno original limpio y uno invertido limpio.
 for (let i = 0; i < stringComprobar.length; i++) {
-  let toArray = stringComprobar[i].replace(",", "").split(" ");
-  for (let y = 0; y < toArray.length; y++) {
-    switch (y) {
-      case 0:
-        drow1.push(toArray[y]);
-        break;
-      case 1:
-        drow2.push(toArray[y]);
-        break;
-      case 2:
-        drow3.push(toArray[y]);
-        break;
-      case 3:
-        drow4.push(toArray[y]);
-        break;
-      case 4:
-        drow5.push(toArray[y]);
-        break;
-      case 5:
-        drow6.push(toArray[y]);
-        break;
-      case 6:
-        drow7.push(toArray[y]);
-        break;
-      case 7:
-        drow8.push(toArray[y]);
-        break;
-      case 8:
-        drow9.push(toArray[y]);
-        break;
-    }
-  }
-  switch (i) {
-    case 0:
-      sdrow.push(drow1);
-      break;
-    case 1:
-      sdrow.push(drow2);
-      break;
-    case 2:
-      sdrow.push(drow3);
-      break;
-    case 3:
-      sdrow.push(drow4);
-      break;
-    case 4:
-      sdrow.push(drow5);
-      break;
-    case 5:
-      sdrow.push(drow6);
-      break;
-    case 6:
-      sdrow.push(drow7);
-      break;
-    case 7:
-      sdrow.push(drow8);
-      break;
-    case 8:
-      sdrow.push(drow9);
-      break;
+  stringComprobarClear.push(
+    stringComprobar[i].replace(/,/g, "").replace(/ /g, "")
+  );
+  stringComprobarClearInvertido.push(
+    stringComprobar[i]
+      .split("")
+      .reverse()
+      .join()
+      .replace(/,/g, "")
+      .replace(/ /g, "")
+      .replace(/!/g, "")
+  );
+}
+
+//Revisar esto, no mira lo que queremos
+for (let i = 0; i < stringComprobarClear.length; i++) {
+  if (stringComprobarClear[i] === stringComprobarClearInvertido[i]) {
+    phraseToCheck.push(stringComprobarClear);
   }
 }
 
-console.log(sdrow);
+console.log(phraseToCheck);
