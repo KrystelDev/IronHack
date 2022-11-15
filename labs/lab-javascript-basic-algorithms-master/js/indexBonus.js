3b1fa67e35caa38573a5b767115637536c0a5dc2
krystel <krystel.dev@gmail.com>
2022-11-15 08:22:12 +0100

    lab js bonus 2/3

--- labs/lab-javascript-basic-algorithms-master/js/indexBonus.js
+++ labs/lab-javascript-basic-algorithms-master/js/indexBonus.js
@@ -1,4 +1,4 @@
-// Quiero saber cuantos caracteres tiene, inclusive espacios etc, cada párrafo
+// Quiero saber cuantos caracteres tiene cada párrafo:
 let pPrimero;
 let pSegundo;
 let pTercero;
@@ -18,3 +18,9 @@ pQTercero = pTercero.length;
 document.body.childNodes[5].innerHTML = `Este parrafo tiene ${pQPrimero} carácteres`;
 document.body.childNodes[9].innerHTML = `Este parrafo tiene ${pQSegundo} carácteres`;
 document.body.childNodes[13].innerHTML = `Este parrafo tiene ${pQTercero} carácteres`;
+
+//Quiero contar cuantas veces sale la palabra "et".
+const arrayPPprimero = pPrimero.split(" ");
+const result = arrayPPprimero.filter((word) => word == "et");
+
+document.body.childNodes[15].innerHTML = `La palabra "et", del latín, aparece ${result.length} veces en estos parrafos.`;
