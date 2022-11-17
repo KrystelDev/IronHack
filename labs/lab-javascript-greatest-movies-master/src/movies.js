ebae249900d751ae3d9f06b3d5a104932523a0b2
krystel <krystel.dev@gmail.com>
2022-11-17 12:11:33 +0100

    lab movies Iteration 2, 3, 4 y 5

--- labs/lab-javascript-greatest-movies-master/src/movies.js
+++ labs/lab-javascript-greatest-movies-master/src/movies.js
@@ -10,19 +10,97 @@ function getAllDirectors(moviesArray) {
 }
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+  // console.log(moviesArray);
+
+  // for (let index = 0; index < moviesArray.length; index++) {
+  //   console.log(moviesArray[index].director);
+  // }
+
+  let result = moviesArray.filter(
+    (movie) =>
+      movie.director == "Steven Spielberg" && movie.genre.includes("Drama")
+  ).length;
+  console.log("Iteration 2->", result);
+  return result;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+  let result;
+  let allScore = 0;
+
+  if (!moviesArray.length) {
+    result = 0;
+  } else {
+    for (let index = 0; index < moviesArray.length; index++) {
+      if (!moviesArray[index].score) {
+        moviesArray[index].score = 0;
+      }
+    }
+
+    for (let index = 0; index < moviesArray.length; index++) {
+      allScore += moviesArray[index].score;
+    }
+
+    result = parseFloat((allScore / moviesArray.length).toFixed(2));
+  }
+  console.log("Iteration 3->", result);
+  return result;
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+  let result;
+  let allDrama = moviesArray.filter((movie) => movie.genre.includes("Drama"));
+  result = scoresAverage(allDrama);
+
+  console.log("Iteration 4->", result);
+  return result;
+}
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-function orderByYear(moviesArray) {}
+function orderByYear(moviesArray) {
+  let result;
+  const arrayNew = [...moviesArray]; // Operador spread (de propagación) en Javascript ES6
+
+  result = arrayNew.sort(function (a, b) {
+    // Le digo que mire únicamente la propiedad year
+    const yearA = a.year;
+    const yearB = b.year;
+    if (yearA < yearB) {
+      return -1;
+    }
+    if (yearA > yearB) {
+      return 1;
+    }
+    if (yearA == yearB) {
+      //En este caso ordenar por titulo
+      const titleA = a.title;
+      const titleB = b.title;
+      if (titleA < titleB) {
+        return -1;
+      }
+      if (titleA > titleB) {
+        return 1;
+      }
+      return 0;
+    }
+  });
+  console.log("Iteration 5->", result);
+  return result;
+}
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-function orderAlphabetically(moviesArray) {}
+function orderAlphabetically(moviesArray) {
+  let result;
+  let arrayNew = moviesArray.slice(0, 19);
+
+  console.log(arrayNew);
+  result = arrayNew;
+
+  return result;
+}
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes(moviesArray) {}
