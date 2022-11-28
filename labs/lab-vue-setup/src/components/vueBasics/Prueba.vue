ad7a30deca5006d0a203c7e73a31116a4256be69
krystel <krystel.dev@gmail.com>
2022-11-28 14:21:37 +0100

    Iteration 2

--- labs/lab-vue-setup/src/components/vueBasics/Prueba.vue
+++ labs/lab-vue-setup/src/components/vueBasics/Prueba.vue
@@ -1,12 +1,17 @@
 <template>
 <div class="aqua">
-    <p>PROBANDO</p>    
+    <p>{{text}}</p>    
 </div>
 
 </template>
 
 <script setup>
+    import {ref} from "vue";
 
+    let probando = true;
+    let text;
+
+    probando ?  text = "Esto es una prueba" : "No podemos ver la prueba";
 </script>
 
 <style>
