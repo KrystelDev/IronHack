b001ab17396590dccdc9830883d7026e73dc9e0d
krystel <krystel.dev@gmail.com>
2022-11-29 19:23:15 +0100

    Iteration 1 | Create a basic template structure inside a new project

--- labs/lab-vue-basics/src/App.vue
+++ labs/lab-vue-basics/src/App.vue
@@ -1,30 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Footer from './components/Footer.vue'
+import NavBar from './components/Navbar.vue'
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <header>
+      <NavBar />
+    </header>
+    
+    <main>
+      NADA
+    </main>
+    
+    <Footer />
+  </div>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 @media (min-width: 1024px) {
---
+++ labs/lab-vue-basics/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+
+<script setup>
+
+</script>
+
+<template>
+    <div class="exercice">
+        <h2>Ejercico 1</h2>
+    </div>
+</template>
+
+<style>
+.exercice{
+    border: solid 1px hsla(160, 100%, 37%, 1);
+}
+</style>
