3fb1d941b3f4bbe82a508122cdbd9e3be4706259
krystel <krystel.dev@gmail.com>
2022-11-30 09:27:05 +0100

    itineracion 5 bonus, style dynamic

--- labs/lab-vue-basics/src/App.vue
+++ labs/lab-vue-basics/src/App.vue
@@ -6,13 +6,50 @@ import Exercice3 from './components/Exercice3.vue'
 import Exercice4 from './components/Exercice4.vue'
 import Footer from './components/Footer.vue'
 
+import {ref} from 'vue'
+
 // Exercici3
 const show = true;
 
-</script>
+// Bonus Exercici5
+// let isChange = ref(true);
 
+// negro
+// let colorFondo = ref('#181818');
+
+// const onChangeColor = () =>{
+//   // colorFondo === '#f8f8f8' ? colorFondo = ref('#181818') : colorFondo = ref('#f8f8f8');
+//   // console.log(colorFondo)
+
+//   if (colorFondo === '#f8f8f8'){
+//     colorFondo = ref('#181818');
+//     console.log("de blanco negro");
+//     console.log(colorFondo.value)
+//   } else {
+//     colorFondo = ref('#f8f8f8');
+//     console.log("de negro a blanco");
+//     console.log(colorFondo.value)
+//   }
+// }
+
+let active = ref(true);
+
+const handleClick = () => {
+  if (active.value){
+    active.value = false;
+    
+    console.log("de blanco negro");
+    console.log(active.value)
+  } else {
+    active.value = true;
+    console.log("de negro a blanco");
+    console.log(active.value)
+  }
+}
+
+</script>                                                    
 <template>
-  <div>
+  <div :style="{ backgroundColor: active ? '#181818' : '#f8f8f8', color: active ? '#f8f8f8' : '#181818' }">
     <header>
       <NavBar />
     </header>
@@ -21,6 +58,7 @@ const show = true;
       <Exercice />
       <Exercice3 v-show="show"></Exercice3>
       <Exercice4 />
+      <button @click="handleClick">Cambiar Fondo</button>
     </main>
     
     <Footer />
@@ -39,14 +77,5 @@ header {
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
